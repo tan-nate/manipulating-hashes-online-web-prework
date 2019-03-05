c13537fd2e6386507c4420066595be62cf088831
Nate Tan <nktan93@gmail.com>
2019-03-05 20:44:44 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,7 @@ def first_challenge
     }
   }
 
-  contacts[:"Freddy Mercury"][:favorite_icecream_flavors].delete_if do |a|
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if do |a|
     a === "strawberry"
   end
 
